292eab2cf37bcf8dfe717e870da211f3c34ceebf
akmaljon1016 <akmaljonsotvoldiev1998@gmail.com>
2021-07-17 12:35:37 -0700

    DistanceTrackerApp

---
+++ app/src/main/java/com/example/distancetrackerapp/Permissions.kt
@@ -0,0 +1,19 @@
+package com.example.distancetrackerapp
+
+import android.Manifest
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.example.distancetrackerapp.Constants.PERMISSION_LOCATION_REQUEST_CODE
+import com.vmadalin.easypermissions.EasyPermissions
+
+object Permissions {
+    fun hasLocationPermission(context: Context)=
+        EasyPermissions.hasPermissions(context,Manifest.permission.ACCESS_FINE_LOCATION)
+
+    fun requestLocationPermission(fragment: Fragment){
+        EasyPermissions.requestPermissions(fragment,
+        "this application cannot work without location permission",
+            PERMISSION_LOCATION_REQUEST_CODE,
+            Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+}
